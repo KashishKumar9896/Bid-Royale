5d35669cd59c6028f1d84e237b625f8a99f74821
Ishit-Singh05 <ishitperihar@gmail.com>
2025-03-10 14:20:20 +0530

    done again

--- server.js
+++ server.js
@@ -17,9 +17,6 @@ const usersFile = path.join(__dirname, 'user.json');
 const morgan = require('morgan');
 app.use(morgan('combined'));
 
-const routes = require('./routes/route');
-app.use('/', routes);
-
 const helmet = require('helmet');
 app.use(helmet());
 
@@ -27,17 +24,23 @@ const cors = require('cors');
 app.use(cors());
 
 
+const routes = require('./routes/route');
+app.use('/', routes);
+
 // Log requests using a direct console log instead of middleware
 app.use((req, res, next) => {
     console.log(chalk.cyan(`[${new Date().toISOString()}] ${req.method} ${req.url}`)); // Log each request
     next();
 });
 
+
 // Route not found handler
 app.use((req, res) => {
     console.log(chalk.yellow(`Route not found for ${req.method} ${req.url}`)); // Log a warning for not found routes
     res.status(404).json({ success: false, message: 'Route not found' });
 });
+
+
 
 // Server Startup
 const PORT = 8090;
